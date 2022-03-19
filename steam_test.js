101447fd65be0eb985029b07e00815e45ac5d1a9
loomkoom <29258685+loomkoom@users.noreply.github.com>
2022-03-20 00:12:52 +0100

    Separated main playtest functions

--- steam_test.js
+++ steam_test.js
@@ -3,10 +3,11 @@ const axios = require("axios");
 const {Client, Intents, MessageEmbed} = require("discord.js")
 const fs = require('fs');
 
+let client = new steamUser();
 const discClient = new Client({
     intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_TYPING]
 })
-discClient.destroy();
+client.setOptions({enablePicsCache: true, picsCacheAll: true, changelistUpdateInterval: 30000});
 discClient.login("NjE5ODk2NTc2NDk5OTA4NjE4.XXO6aQ.FPHqOIF5aX9gyhI3l4N0F0FLuls")
     .then(() => {
         console.info('[DISCORD] ->   logged in as ' + discClient.user.tag);
@@ -40,7 +41,7 @@ fs.readFile('key.txt', 'utf8', function (err, data) {
 });
 
 
-client.setOptions({enablePicsCache: true, picsCacheAll: true, changelistUpdateInterval: 30000});
+let g_sessionID;
 
 
 var g_sessionID;
@@ -48,12 +49,12 @@ client.on('webSession', function (sessionID, cookies) {
     g_sessionID = sessionID;
     axios.defaults.headers.Cookie = cookies
     console.log("Session: ", sessionID);
-
+});
 
 })
 ;
 
-client.on('accountInfo', () => {
+client.on('loggedOn', () => {
     console.log(`[STEAM]   ->   ${client.vanityURL} (${client.steamID.getSteamID64()}, ${client.steamID.getSteam2RenderedID()}, ${client.steamID.getSteam3RenderedID()})`);
 });
 
@@ -68,8 +69,12 @@ client.on('changelist', function (changeNumber, changeApps, changePackages) {
     fs.writeFile('change.txt', changeNumber.toString(), err => {
     })
     console.log(`-- Change - NEW: ${changeNumber}`);
-    let msg = "---BETA\n"
-    client.getProductInfo(changeApps, changePackages, {}, (err, apps, packages, unknownApps, unknownPackages) => {
+    fetchChanges(changeNumber, changeApps, changePackages)
+});
+
+function fetchChanges(changeNumber, changeApps, changePackages) {
+    client.getProductInfo(changeApps, changePackages).then(result => {
+        let apps = result.apps;
         let out = [`*Changelist - <https://steamdb.info/changelist/${changeNumber}> (${changeApps.length} apps and ${changePackages.length} packages)`];
         let beta = []
         if (apps && Object.keys(apps).length) {
@@ -82,97 +87,65 @@ client.on('changelist', function (changeNumber, changeApps, changePackages) {
                     const embed = new MessageEmbed()
                         .addFields(
                             {name: `${title} (${app})`, value: `Parent:\t${parent}`},
-                            {name: '\u200B', value: '\u200B'},
-                        )
-                    var active = false;
-                    axios.get(`https://store.steampowered.com/app/${parent}`).then(
-                        resp => active = resp.data.indexOf('RequestPlaytestAccess') > 0);
-                    if (active) {
-                        embed.addField('Store Button', 'Active')
-                        axios.post('https://store.steampowered.com/ajaxrequestplaytestaccess/' + app,
-                            `sessionid=${g_sessionID}`,
-                            {
-                                validateStatus: (status) => {
-                                    return (status >= 200 && status < 400 || status === 401);
-                                }
-                            })
-                            .then(function (response) {
-                                let responseText = response.data
-                                let granted;
-                                let success;
-                                if (!responseText === false) {
-                                    granted = responseText.granted;
-                                    success = responseText.success;
-                                }
-                                if (granted === 1) {
-                                    console.log(`New playtest requested: \n\t${title} \n\tparent: ${app}\n\t=> Instant Access`);
-                                    embed.addField('New playtest requested', 'Instant Access')
-                                } else if (granted === null) {
-                                    console.log(`New playtest requested: \n\t${title} \n\tparent: ${app}\n\t=> Requested Access`);
-                                    embed.addField('New playtest requested', 'Requested Access')
-                                } else if (!success || success !== 1) {
-                                    console.warn(`FAILED playtest request: \n\t${title} \n\tparent: ${app}\n\t=> response : ${responseText} -  (${response.status}: ${response.statusText})`);
-                                }
-                                embed.addField('Links', `[Store](https://store.steampowered.com/app/${app}) [SteamDB](https://steamdb.info/app/${app})`)
+                            {name: '\u200B', value: '\u200B'})
+                    axios.get(`https://store.steampowered.com/app/${parent}`).then(resp => {
+                        let active = resp.data.indexOf('RequestPlaytestAccess') > 0;
 
-                            })
-                            .catch(function (error) {
-                                console.warn(error);
-                            });
-                    } else {
-                        embed.addField('Store Button', 'None')
-                    }
-                    discClient.channels.resolve(276023946657136640).fetch(true)
-                        .then(channel => channel.send({embeds: [embed]}));
+                        embed.addField('Store Button: ', `${"Yes" ? active : "No"}`)
+                        console.log(app in client.getOwnedApps(), app in client.picsCache.apps)
+                        if (active && !(app in client.getOwnedApps())) {
+                            requestPlaytest(app, parent, title, embed);
+                        }
+                        discClient.channels.fetch(276023946657136640)
+                            .then(channel => channel.send({embeds: [embed]}));
+                    });
                 }
             }
         }
 
-        if (packages && Object.keys(packages).length) {
-            for (let pack of Object.keys(packages)) {
-                out.push(` - package: <https://steamdb.info/sub/${pack}> - ${packages[pack].packageinfo ? (packages[pack].packageinfo.name || 'Unknown') : 'Unknown App'}`);
+        if (result.packages && Object.keys(result.packages).length) {
+            for (let pack of Object.keys(result.packages)) {
+                out.push(` - package: <https://steamdb.info/sub/${pack}> - ${result.packages[pack].packageinfo ? (result.packages[pack].packageinfo.name || 'Unknown') : 'Unknown App'}`);
             }
         }
 
-        if ((unknownApps && unknownApps.length) || (unknownPackages && unknownPackages.length)) {
-            out.push(`${unknownApps.length} Unknown Apps and ${unknownPackages.length} Unknown Packages`);
+        if ((result.unknownApps && result.unknownApps.length) || (result.unknownPackages && result.unknownPackages.length)) {
+            out.push(`${result.unknownApps.length} Unknown Apps and ${result.unknownPackages.length} Unknown Packages`);
         }
 
         console.log(out.join('\n'));
         console.log(beta.join('\n'));
     });
-});
+}
 
-
-/* GREASEMONKEY
-    GM_xmlhttpRequest ({
-        method: "POST",
-        url: 'https://store.steampowered.com/ajaxrequestplaytestaccess/'+appid,
-        data: "sessionid=" + g_sessionID,
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        onload: function(response)
-        {console.log("Response: ",response)}
- */
-
-/* BROWSER
-    var req = new XMLHttpRequest();
-    req.onload = response =>
-            {console.log(response)};
-    req.open("POST",
-    'https://store.steampowered.com/ajaxrequestplaytestaccess/'+1925270);
-    req.setRequestHeader( "Content-Type", "application/x-www-form-urlencoded");
-    req.send("sessionid="+g_sessionID);
- */
-
-/* AXIOS
-    axios.defaults.headers.Cookie = g_cookies;
-    axios.post("https://store.steampowered.com/ajaxrequestplaytestaccess/" + appid,
-        `sessionid=${g_sessionID}`,
-        {
+function requestPlaytest(app, parent, title, embed) {
+    console.log('Store Button: ', 'Active')
+    axios.post('https://store.steampowered.com/ajaxrequestplaytestaccess/' + parent, `sessionid=${g_sessionID}`, {
+        validateStatus: (status) => {
+            return (status >= 200 && status < 400 || status === 401);
+        }
     })
-        .then(resp => {
-            console.log("Response: ",resp.status,"\n",resp.headers,resp.config,"\n",resp.data)
+        .then(function (response) {
+            let responseText = response.data
+            let granted;
+            let success;
+            if (!responseText === false) {
+                granted = responseText.granted;
+                success = responseText.success;
+            }
+            if (granted === 1) {
+                console.log(`New playtest requested (${app}) : \n\t${title} \n\tparent: ${parent}\n\t=> Instant Access`);
+                embed.addField('New playtest requested', 'Instant Access')
+            } else if (granted === null) {
+                console.log(`New playtest requested (${app}): \n\t${title} \n\tparent: ${parent}\n\t=> Requested Access`);
+                embed.addField('New playtest requested', 'Requested Access')
+            } else if (!success || success !== 1) {
+                console.warn(`FAILED playtest request (${app}): \n\t${title} \n\tparent: ${parent}\n\t=> response : ${responseText} -  (${response.status}: ${response.statusText})`);
+            }
+            embed.addField('Links: ', `[Store](https://store.steampowered.com/app/${app}) | [SteamDB - parent](https://steamdb.info/app/${parent}) | [SteamDB - app](https://steamdb.info/app/${app})`)
+
         })
- */
+        .catch(function (error) {
+            console.warn(error);
+        });
+}
